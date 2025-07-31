dadbce1c52cf05657357a2eb2126ca634d10e91c
TrennyDevelopement <asiasartofficial@gmail.com>
2025-07-31 16:38:15 +0530

    Create key.js

---
+++ api/key.js
@@ -0,0 +1,38 @@
+import { randomBytes } from 'crypto';
+import mongoose from 'mongoose';
+
+const mongoUri = "mongodb+srv://stuntmanxbunny:bunny@asiaartsdb1.hp6gt.mongodb.net/?retryWrites=true&w=majority&appName=AsiaArtsDB1"
+
+
+const apiKeySchema = new mongoose.Schema({
+  key: String,
+  createdAt: { type: Date, default: Date.now }
+});
+const ApiKey = mongoose.models.ApiKey || mongoose.model('ApiKey', apiKeySchema);
+
+async function connect() {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  }
+}
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Only POST allowed' });
+  }
+
+  try {
+    await connect();
+
+    const key = randomBytes(16).toString('hex');
+    const newKey = await ApiKey.create({ key });
+
+    return res.status(200).json({ key: newKey.key });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal error' });
+  }
+}
