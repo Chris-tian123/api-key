994c0e8273f283b6f960ac11c8d6496b9fa68fe1
TrennyDevelopement <asiasartofficial@gmail.com>
2025-07-31 15:46:26 +0530

    Update get-keys.js

--- api/get-keys.js
+++ api/get-keys.js
@@ -3,7 +3,7 @@ import Key from '../../models/Key';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Only GET allowed' });
-  if (req.headers['x-admin-token'] !== process.env.ADMIN_TOKEN) return res.status(403).json({ error: 'Forbidden' });
+  if (req.headers['x-admin-token'] !== "trelloxbunny") return res.status(403).json({ error: 'Forbidden' });
 
   await connectDB();
   const keys = await Key.find({});
